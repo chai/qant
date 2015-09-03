746fe6c74115869668dbbfcbb8a6387eedcea18c
chai <chaihongnge@gmail.com>
2015-09-03 14:33:24 +0900

    Add empty clam and rating code

--- Clam.swift
+++ Clam.swift
@@ -10,12 +10,67 @@ import UIKit
 
 class Clam: UIView {
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+ 
+    //MARK: Properties
+    
+    
+    var rating = 0
+    var clamsButtons = [UIButton]()
+
+    //set clams size to be relative to the frame
+    
+    
+    //MARK: Initilisation
+    
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+        
+        let filledClam = UIImage(named: "clam")
+        let emptyClam = UIImage (named: "emptyClam")
+        
+
+       
+        for _ in 0..<5 {
+            let button = UIButton()
+            button.backgroundColor = UIColor.redColor()
+            button.addTarget(self, action: "clamButtonTapped:", forControlEvents: .TouchDown)
+            clamsButtons += [button]
+            addSubview(button)
+        }
+        
     }
-    */
+    
+    
+    override func layoutSubviews() {
+    
+        let buttonSize = Int(frame.size.height)
+        
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+        
+        // Offset each button's origin by the length of the button plus spacing.
+        for (index, button) in clamsButtons.enumerate() {
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + 5))
+            button.frame = buttonFrame
+        }
+        
+
+    }
+    
+    
+    //MARK: Button Action
+    
+    
+    
+    func clamButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
+    
+
+    
+    
+    
+    
+    
+    
 
 }
