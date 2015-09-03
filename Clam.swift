7cb45ef1c18a5c464dc3c50926ab2addf510d354
chai <chaihongnge@gmail.com>
2015-09-03 11:51:45 +0900

    Add navigations. prototype cell, and start on clams

---
+++ Clam.swift
@@ -0,0 +1,21 @@
+//
+//  Clam.swift
+//  qant
+//
+//  Created by Chai on 9/3/15.
+//  Copyright © 2015 Chai. All rights reserved.
+//
+
+import UIKit
+
+class Clam: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
